1a86bd71f1a813b42b80026bad88d6e92b446778
CodingCat <zhunan@zhunan-2.local>
2013-09-20 20:37:07 -0400

    itinerary

--- src/server/ResImpl/NIOResourceManagerImpl.java
+++ src/server/ResImpl/NIOResourceManagerImpl.java
@@ -324,7 +324,11 @@ public class NIOResourceManagerImpl extends NIOResourceManager {
     @Override
     public boolean itinerary(int id, int customer, Vector flightNumbers,
                              String location, boolean Car, boolean Room) {
-        //TODO: to implement
+        for (Object obj : flightNumbers) {
+            reserveFlight(id, customer, (Integer) obj);
+        }
+        if (Car) reserveCar(id, customer, location);
+        if (Room) reserveRoom(id, customer, location);
         return false;
     }
 
